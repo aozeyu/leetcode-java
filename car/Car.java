ff07402318827ec04f53737e99b9fec2a4eb864e
aozeyu <3484766443@qq.com>
2025-04-04 21:00:19 +0800

    add code

---
+++ car/Car.java
@@ -0,0 +1,21 @@
+package car;
+
+public class Car {
+  private String description;
+
+  public Car(String description) {
+    this.description = description;
+  }
+  public void startEngine(){
+    System.out.println("Car -> startEngine");
+  }
+
+  protected void runEngine(){
+    System.out.println("Car -> runEngine");
+  }
+
+  public void drive(){
+    System.out.println("Car -> drive, type is " + getClass().getSimpleName());
+    runEngine();
+  }
+}
