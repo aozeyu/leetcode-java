9e4a7f64515d5ebaf4ea9741ff0542c09932c286
aozeyu <3484766443@qq.com>
2025-04-19 12:33:39 +0800

    add code

--- Main.java
+++ Main.java
@@ -5,6 +5,19 @@ public class Main {
   public static void main(String[] args) {
     for (DayOfTheweek dayOfTheweek : DayOfTheweek.values()) {
       System.out.println(dayOfTheweek.name() + " : " + dayOfTheweek.getPrice());
+      Boolean b = true;
+      if (b) {
+
+      }
+      int foo = 100;
+      for (int i = 0; i < foo; i++) {
+
+      }
+
+      int[] a = new int[]{10,20,30};
+      for (int i = 0; i < a.length; i++) {
+        
+      }
     }
   }
 }
